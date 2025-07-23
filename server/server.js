79d578c1d66a0d9bfb511f00896585a4194d83c7
Arsen <kazaktars123@gmail.com>
2025-07-23 11:05:41 +0500

    Connected Mongoose in server

--- server/server.js
+++ server/server.js
@@ -1,13 +1,26 @@
 const express = require("express")
+const mongoose = require("mongoose")
 const cors = require("cors")
+require("dotenv").config()
+
 const app = express()
 
 
 app.use(cors())
 app.use(express.json())
 
-app.get("/test", (req,res) => {
+
+mongoose.connect(`${process.env.MONGO_URL}`)
+    .then(() => console.log("MongoDB is connected"))
+    .catch(() => console.log(err))
+
+
+app.get("/tes", (req,res) => {
     res.json({message: 'Server is running'})
 })
 
-app.listen(5000, () =>  console.log("Server is running on 5000 port"))
+
+app.listen(process.env.PORT, () =>  
+    console.log(`Server is running on ${process.env.PORT} port`
+))
+
